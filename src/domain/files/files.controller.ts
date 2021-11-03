a14ee184a3790ea5f9bfca14ed834d5e82aeeb5d
龙骑将杨影枫 <catwindboy@hotmail.com>
2021-11-03 10:28:23 +0800

    feature: 完善上传 模块

--- src/domain/files/files.controller.ts
+++ src/domain/files/files.controller.ts
@@ -7,15 +7,24 @@ import {
 import { FileInterceptor } from '@nestjs/platform-express';
 import { ApiTags } from '@nestjs/swagger';
 import * as path from 'path';
+import { execSync } from 'child_process';
 @ApiTags('文件上传')
 @Controller('files')
 export class FilesController {
   @Post('upload')
   @UseInterceptors(FileInterceptor('file'))
   async uploadFiles(@UploadedFile() file) {
-    console.log(__dirname);
-    const savedPath = path.join(__dirname, '../../../static');
-    console.log(savedPath);
-    return { file };
+    // console.log(__dirname);
+    // const savedPath = path.join(__dirname, '../../../static');
+    // console.log(savedPath);
+    // const { originalname = '' } = file || { originalname: '' };
+    // console.log(file);
+    // const filepath = `${savedPath}/${originalname}`;
+    console.log(file);
+    const filepath = path.join(__dirname, `../../../${file.path}`);
+    console.log(filepath);
+    const code = execSync('node -v', { stdio: 'inherit' });
+    console.log(code);
+    return { path: filepath };
   }
 }
--- src/domain/files/files.module.ts
+++ src/domain/files/files.module.ts
@@ -1,8 +1,25 @@
 import { Module } from '@nestjs/common';
 import { FilesController } from './files.controller';
 import { FilesService } from './files.service';
-
+import { MulterModule } from '@nestjs/platform-express';
+import dayjs = require('dayjs');
+import { diskStorage } from 'multer';
+import * as nuid from 'nuid';
+import * as path from 'path';
 @Module({
+  imports: [
+    MulterModule.register({
+      storage: diskStorage({
+        destination: `./static/${dayjs().format('YYYY-MM-DD')}`,
+        filename: (req, file, cb) => {
+          // 自定义文件名
+          const filename = `${nuid.next()}`;
+          return cb(null, `${filename}-${file.originalname}`);
+          // return cb(null, file.originalname);
+        },
+      }),
+    }),
+  ],
   controllers: [FilesController],
   providers: [FilesService],
 })
